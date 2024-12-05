cc08d9ccc33e9fa5290031062f2a691413c67722
xertrec <alillolindo@gmail.com>
2024-12-05 16:20:16 +0100

    Code organization

--- classes/GestioVista.php
+++ classes/GestioVista.php
@@ -4,33 +4,43 @@ header('Content-Type: text/html; charset=UTF-8');
 include_once ("temporada.php");
 include_once("valoracions.php");
 include_once("Vista.php");
-?>
-<center>
-<?php
 
-    if (isset($_POST["nomSerie"])) {
-        ?>
-        <title>Llistat de temporades</title>
-        <?php
-        $nomSerie = $_POST["nomSerie"];
-        $t = new Temporada();
-        $llistaTemporades = $t->llistatTemporades($nomSerie);
-        $v = new Vista();
-        $v->mostrarLlistatTemporades($llistaTemporades);
-        echo '<br><a href="../index.html"> Tornar </a>';
-    }
+if (isset($_POST["opcio"])) {
 
-    if (isset($_POST["nomSerie_val"])){
-        ?>
-            <title>Llistat de valoracions de temporades</title>
-        <?php
-        $nomSerie_val = $_POST["nomSerie_val"];
-        $numTemporada = $_POST["numTemporada"];
-        $t = new Valoracio();
-        $llistaValoracions = $t->llistatValoracions($nomSerie_val, $numTemporada);
-        $v = new Vista();
-        $v->mostrarLlistatValoracions($llistaValoracions);
-        echo '<br><a href="../index.html"> Tornar </a>';
+    $opcio = $_POST["opcio"];
+	$v = new Vista();
+    switch ($opcio) {
+        case 'Veure temporades': {
+            if (isset($_POST["nomSerie"])) {
+                $nomSerie = $_POST["nomSerie"];
+                $t = new Temporada();
+                $llistaTemporades = $t->llistatTemporades($nomSerie);
+
+                $v->mostrarCapsalera("Llistat de temporades");
+                $v->mostrarLlistatTemporades($llistaTemporades);
+                $v->mostrarPeu();
+            }
+            break;
+        }
+
+        case 'Veure valoracions': {
+            if (isset($_POST["nomSerie"]) and isset($_POST["numTemporada"])) {
+                $nomSerie = $_POST["nomSerie"];
+                $numTemporada = $_POST["numTemporada"];
+                $t = new Valoracio();
+                $llistaValoracions = $t->llistatValoracions($nomSerie, $numTemporada);
+
+                $v->mostrarCapsalera("Llistat de valoracions de temporades");
+                $v->mostrarLlistatValoracions($llistaValoracions);
+                $v->mostrarPeu();
+            }
+            break;
+        }
+        
+        case 'Valorar': {
+            break;
+        }
     }
+}
+
 ?>
-</center>
--- classes/vista.php
+++ classes/vista.php
@@ -15,6 +15,21 @@ class Vista{
         }
     }
 
+    public function mostrarCapsalera($titol) {
+        echo ('<!DOCTYPE HTML><html>
+                    <head>
+                        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                        <title>' . $titol . '</title>
+                    </head>
+                    <body>
+                        <center>
+                        <br> <h1>' . $titol . '</h1><br><br>');
+    }
+
+    public function mostrarPeu() {
+        echo ('<br><a href="../index.html"> Tornar </a></center></body></html>');
+    }
+
     public function mostrarLlistatSeries ($llistaSeries, $triar) {
         $this->abd->connectarBD();
         $res="<table border=1><tr bgcolor='lightgray'>
@@ -50,10 +65,8 @@ class Vista{
         return $res;
     }
 
-    public function mostrarDropdownSeries ($llistaSeries) {
-        $res = "<form action='./classes/GestioVista.php' method='POST'>";
-
-        $res = $res . "<label for='serieDropdown'>Selecciona una sèrie:&nbsp;</label>";
+    public function mostrarSelectorSeries ($llistaSeries) {
+        $res = "<label for='serieDropdown'>Selecciona una sèrie:&nbsp;</label>";
         $res = $res . "<select id='serieDropdown' name='nomSerie'>";
         $res = $res . "<option selected='true' disabled='disabled'>Selecciona una opción</option>";
         
@@ -62,21 +75,10 @@ class Vista{
         }
 
         $res = $res . "</select>";
-
-        $res = $res . "<input type='submit' value='Enviar'>";
-        $res = $res . "</form>";
         echo ($res);
     }
 
     public function mostrarLlistatTemporades ($llistatTemporades) {
-        echo "<!DOCTYPE html>";
-        echo "<html>";
-        echo "<head>";
-        echo "<meta charset='utf-8'>";
-        echo "<title>Llistat de temporades</title>";
-        echo "</head>";
-        echo "<body>";
-        echo "<h1>Llistat de temporades</h1>";
 
         $res="<table border=1><tr bgcolor='lightgray'>
                             <th>Nom Serie</th>
@@ -104,16 +106,7 @@ class Vista{
         echo ($res);
     }
 
-
     public function mostrarLlistatValoracions ($llistatValoracions){
-        echo "<!DOCTYPE html>";
-        echo "<html>";
-        echo "<head>";
-        echo "<meta charset='utf-8'>";
-        echo "<title>Llistat de valoracions de temporades</title>";
-        echo "</head>";
-        echo "<body>";
-        echo "<h1>Llistat de valoracions de temporades</h1>";
 
         $res="<table border=1><tr bgcolor='lightgray'>
                             <th>Nom Serie</th>
@@ -139,67 +132,6 @@ class Vista{
             $res .= "</tr>";
         }
         $res = $res . "</table>";
-        echo ($res);
-    }
-
-    public function mostrarDropdownValoracions ($llistaSeries) {
-        $res = "<form action='./classes/GestioVista.php' method='POST'>";
-
-        $res = $res . "<label for='valoracioDropdown'>Selecciona una sèrie:</label>";
-        $res = $res . "<select id='valoracioDropdown' name='nomSerie_val'>";
-        $res = $res . "<option selected='true' disabled='disabled'>Selecciona una opción</option>";
-
-        foreach ($llistaSeries as $serie) {
-            $res = $res . "<option value='" . $serie['nomSerie'] . "'>" . $serie['nomSerie'] . "</option>";
-        }
-
-        $res = $res . "</select>";
-
-
-        $res = $res .  "<br><br><label for='numTemporada'>Número de temporada:</label>
-                        <input type='number' id='numTemporada' name='numTemporada' required />";
-
-        $res = $res . "<input type='submit' value='Enviar'>";
-        $res = $res . "</form>";
-        echo ($res);
-    }
-
-    public function mostrarQuestionariValorar ($llistaSeries) {
-        $res = "<form action='./classes/GestioVista.php' method='POST'>";
-
-        $res = $res . "<label for='serieDropdown'>Selecciona una sèrie: &nbsp;</label>";
-        $res = $res . "<select id='serieDropdown' name='nomSerie'>";
-        $res = $res . "<option selected='true' disabled='disabled'>Selecciona una opción</option>";
-        
-        foreach ($llistaSeries as $serie) {
-            $res = $res . "<option value='" . $serie['nomSerie'] . "'>" . $serie['nomSerie'] . "</option>";
-        }
-
-        $res = $res . "</select>";
-        $res = $res . "<br><br>";
-
-        $res = $res . "<label for='numTemporada'>Nombre de temporada: &nbsp;</label>";
-        $res = $res . "<input type='number' id='numTemporada' name='numTemporada' required />";
-        $res = $res . "<br><br>";
-
-        $res = $res . "<label for='nomUsuari'>Nom d'usuari: &nbsp;</label>";
-        $res = $res . "<input type='text' id='nomUsuari' name='nomUsuari' required />";
-        $res = $res . "<br><br>";
-
-        $res = $res . "<label for='contrasenya'>Contrasenya: &nbsp;</label>";
-        $res = $res . "<input type='text' id='contrasenya' name='contrasenya' required />";
-        $res = $res . "<br><br>";
-
-        $res = $res . "<label for='valoracio'>Valoració: &nbsp;</label>";
-        $res = $res . "<input type='number' id='valoracio' name='valoracio' required />";
-        $res = $res . "<br><br>";
-
-        $res = $res . "<label for='comentari'>Comentari: &nbsp;</label>";
-        $res = $res . "<input type='text' id='comentari' name='comentari' required />";
-        $res = $res . "<br><br>";
-
-        $res = $res . "<input type='submit' value='Enviar'>";
-        $res = $res . "</form>";
         echo ($res);
     }
 }
