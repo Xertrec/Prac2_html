263f3a0e57ea812aeb54f416b94b18bbce005450
Alberto <40476542+xBlanco@users.noreply.github.com>
2024-12-20 18:25:26 +0100

    Update control.php

--- classes/control.php
+++ classes/control.php
@@ -5,6 +5,8 @@ header("Content-Type: text/html;charset=utf-8");
 include_once ("Serie.php");
 include_once ("Temporada.php");
 include_once("Valora.php");
+include_once ("Usuari.php");
+include_once ("GestioVista.php");
 
 class Control {
 	
@@ -49,6 +51,29 @@ class Control {
 		$v = new Valoracio();
 		$res = $v->valorarTemporada($nomSerie, $numTemporada, $nomUsuari, $contrasenya, $valor, $comentari);
 		
+		return($res);
+	}
+
+	//___
+
+	public function desbloquejarUsuari($nomUsuari, $contrasenya){
+		$res = "";
+		$u = new Usuari();
+		$res = $u->desbloquejarUsuari($nomUsuari, $contrasenya);
+		return($res);
+	}
+
+	public function llistatUsuariBloquejat() {
+		$res = "";
+		$u = new Usuari();
+		$res = $u->llistatUsuariBloquejat();
+		return($res);
+	}
+
+	public function desbloquejar($nomUsuari){
+		$res = "";
+		$u = new Usuari();
+		$res = $u->desbloquejar($nomUsuari);
 		return($res);
 	}
 }
